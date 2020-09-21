e0ba77131b87090d95d4d030c3c0f2db21be325e
Raylands <minestylecs@gmail.com>
2020-09-21 12:19:29 +0200

    Changed member properties.

--- Lecture-1/Exercise2/Student.h
+++ Lecture-1/Exercise2/Student.h
@@ -4,14 +4,15 @@
 using namespace std;
 
 class Student {
-public:
-	Student(int ID, string name, int subject1, int subject2, int subject3);
-
+private:
 	int ID;
 	string name;
 	int subject1;
 	int subject2;
 	int subject3;
+
+public:
+	Student(int ID, string name, int subject1, int subject2, int subject3);
 
 	void getStudent();
 	char totalMarks();
