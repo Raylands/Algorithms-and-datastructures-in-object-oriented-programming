a323684c5bac0569cad5b004d6240eaf3d708087
Raylands <minestylecs@gmail.com>
2020-09-21 14:21:57 +0200

    Improved randomness.

--- Lecture-2/Problem 1/Vector_shuffle.cpp
+++ Lecture-2/Problem 1/Vector_shuffle.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 
 template <class T>
-void shuffleVector(vector<T>& Vector, int rotations);
+void shuffleVector(vector<T>& Vector, const int rotations);
 template <class T>
 void printVector(vector<T>& Vector);
 
 template <class T>
-void shuffleVector(vector<T>& Vector, int rotations = 1) {
+void shuffleVector(vector<T>& Vector, const int rotations = 1) {
+    srand(time(0));
     for (size_t i = 0; i < rotations; i++)
     {
         for (int j = 0; j < Vector.size(); j++)
@@ -26,9 +28,9 @@ template <class T>
 void printVector(vector<T>& Vector) {
     for (int i = 0; i < Vector.size(); i++)
     {
-        cout << Vector[i] << endl;
+        cout << Vector[i] << ' ';
     }
-    cout << endl;
+    cout << endl << endl;
 }
 
 int main()
@@ -36,5 +38,4 @@ int main()
     vector<int> testVector{ 1, 2, 3, 4, 5, 6};
 
     shuffleVector(testVector,5);
-
 }
