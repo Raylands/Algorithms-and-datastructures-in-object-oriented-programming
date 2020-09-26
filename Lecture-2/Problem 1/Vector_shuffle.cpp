be60ab78a76508f8983990d554a903c07959f0fd
Raylands <minestylecs@gmail.com>
2020-09-26 10:15:20 +0200

    Added ability to toggle generics.

--- Lecture-2/Problem 1/Vector_shuffle.cpp
+++ Lecture-2/Problem 1/Vector_shuffle.cpp
@@ -4,38 +4,72 @@
 
 using namespace std;
 
-template <class T>
-void shuffleVector(vector<T>& Vector, const int rotations);
-template <class T>
-void printVector(vector<T>& Vector);
+#define GENERIC true
 
-template <class T>
-void shuffleVector(vector<T>& Vector, const int rotations = 1) {
-    srand(time(0));
-    for (size_t i = 0; i < rotations; i++)
-    {
-        for (int j = 0; j < Vector.size(); j++)
+#if GENERIC
+    template <typename T>
+    void shuffleVector(vector<T>& Vector, const int rotations);
+    template <typename T>
+    void printVector(vector<T>& Vector);
+#else
+    void shuffleVector(vector<int>& Vector, const int rotations);
+    void printVector(vector<int>& Vector);
+#endif // GENERIC
+
+
+
+#if GENERIC
+    template <typename T>
+    void shuffleVector(vector<T>& Vector, const int rotations = 1) {
+        srand(time(0));
+        for (size_t i = 0; i < rotations; i++)
         {
-            int random = rand() % Vector.size();
+            for (int j = 0; j < Vector.size(); j++)
+            {
+                int random = rand() % Vector.size();
 
-            swap(Vector[j], Vector[random]);
+                swap(Vector[j], Vector[random]);
+            }
+            printVector(Vector);
         }
-        printVector(Vector);
     }
-}
 
-template <class T>
-void printVector(vector<T>& Vector) {
-    for (int i = 0; i < Vector.size(); i++)
-    {
-        cout << Vector[i] << ' ';
+    template <typename T>
+    void printVector(vector<T>& Vector) {
+        for (int i = 0; i < Vector.size(); i++)
+        {
+            cout << Vector[i] << ' ';
+        }
+        cout << endl << endl;
     }
-    cout << endl << endl;
-}
+#else
+    void shuffleVector(vector<int>& Vector, const int rotations = 1) {
+        srand(time(0));
+        for (size_t i = 0; i < rotations; i++)
+        {
+            for (int j = 0; j < Vector.size(); j++)
+            {
+                int random = rand() % Vector.size();
+
+                swap(Vector[j], Vector[random]);
+            }
+            printVector(Vector);
+        }
+    }
+
+    void printVector(vector<int>& Vector) {
+        for (int i = 0; i < Vector.size(); i++)
+        {
+            cout << Vector[i] << ' ';
+        }
+        cout << endl << endl;
+    }
+#endif // GENERIC
 
 int main()
 {
-    vector<int> testVector{ 1, 2, 3, 4, 5, 6};
+    //vector<int> testVector{ 1, 2, 3, 4, 5, 6};
+    vector<long> testVector{ 1, 2, 3, 4, 5, 6};
 
     shuffleVector(testVector,5);
 }
