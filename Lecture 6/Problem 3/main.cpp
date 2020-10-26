ed878cae48f3c8dcb9f6939bbb2786b571d5d3e2
Raylands <minestylecs@gmail.com>
2020-10-26 14:43:29 +0100

    Implementet Parentheses test.

---
+++ Lecture 6/Problem 3/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool fine(vector<char> vector);
+
+int main()
+{
+    vector<char> vector { '(', '(', ')', '(',')', ')', '{', '}' };
+
+    cout << (fine(vector) ? "Everything is fine!" : "Something is missing!") << endl;
+}
+
+bool fine(vector<char> vector)
+{
+    int parentheses = 0, brackets = 0, braces = 0;
+    for (auto item : vector)
+    {
+        switch (item)
+        {
+        case '(':
+            parentheses++;
+            break;
+        case '[':
+            brackets++;
+            break;
+        case '{':
+            braces++;
+            break;
+        case ')':
+            parentheses--;
+            break;
+        case ']':
+            brackets--;
+            break;
+        case '}':
+            braces--;
+            break;
+        }
+    }
+    if (parentheses || brackets || braces) return false;
+    else return true;
+}
