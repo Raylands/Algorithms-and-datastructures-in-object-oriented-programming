2bc33f777b6a264daf9aa35c14506914e7c4bb03
Raylands <minestylecs@gmail.com>
2020-10-26 15:05:40 +0100

    Implemented logarithm function with base 2.

---
+++ Lecture 6/Problem 2/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int log2(int number);
+
+int main()
+{
+    int number;
+    cout << "Input number for log2 calculation: ";
+    cin >> number;
+    cout << log2(number);
+}
+
+int log2(int number)
+{
+    if (number == 1)
+        return 0;
+    return 1 + log2(number / 2);
+}
