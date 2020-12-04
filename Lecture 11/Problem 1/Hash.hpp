3dc1ac7ebb60924ecd028641d2275ab3edbd681e
Raylands <minestylecs@gmail.com>
2020-12-04 20:10:57 +0000

    Hash function works now.

--- Lecture 11/Problem 1/Hash.hpp
+++ Lecture 11/Problem 1/Hash.hpp
@@ -6,21 +6,21 @@
 
 using namespace std;
 
-struct pairHash : unary_function<const pair<string, Reason>&, int> {
-    size_t operator()(argument_type arg) const {
+struct pairHash {
+    size_t operator()(const string& arg) const {
         unsigned int h = 0;
-        for (int i = 0; i < arg.first.length(); i++)
+        for (int i = 0; i < arg.length(); i++)
         {
             h = (h << 3) | (h >> 5);
-            h += (unsigned int)arg.first[i];
+            h += (unsigned int)arg[i];
         }
         return h;
     }   
 };
 
-struct pairEquals : binary_function<const pair<string, Reason>&, const pair<string, Reason>&, bool> {
+struct pairEquals : binary_function<const string, const string, bool> {
     result_type operator()(first_argument_type lhs, second_argument_type rhs) const
     {
-        return (lhs.first == rhs.first) && (lhs.second == rhs.second);
+        return lhs == rhs;
     }
 };
--- Lecture 11/Problem 1/Problem 1.cpp
+++ Lecture 11/Problem 1/Problem 1.cpp
@@ -11,11 +11,11 @@ int main()
 {
     unordered_map <string, Reason, pairHash, pairEquals> client_reasons;
     
-    client_reasons.insert(make_pair("Name1", Reason::Leasing));
-    //client_reasons.insert({ "Name2", Reason::Insurance });
+    client_reasons.insert({ "Name1", Reason::Leasing });
+    client_reasons.insert({ "Name2", Reason::Insurance });
 
-    //cout << "What does Name2 want from us? " << enum_strings[(int)client_reasons.find("Name2")->second] << endl;
+    cout << "What does Name2 want from us? " << enum_strings[(int)client_reasons.find("Name2")->second] << endl;
 
-    //pair<string, Reason> a("Name1", Reason::Insurance);
-    //cout << client_reasons.hash_function()(a) << endl;
+    cout << "Name1: " << client_reasons.hash_function()("Name1") << endl;
+    cout << "ame1N: " << client_reasons.hash_function()("ame1N") << endl;
 }
